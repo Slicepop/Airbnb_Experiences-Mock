edbfc67600fa929ab694a69cfb9732e86b7e7349
Slicepop <46600908+Slicepop@users.noreply.github.com>
2023-07-20 09:28:26 -0400

    add everything else

---
+++ AirbnbExperiencesMock/src/components/Hero.jsx
@@ -0,0 +1,12 @@
+export default function Hero(){
+    return (
+        <div className="heroDiv">
+            <div className="heroimgDiv">
+                <img src= './src/assets/photo-grid.png' className="hero"></img>
+            </div>
+                <h1>Online Experiences</h1>
+                <p>Join unique interactive activities led by one-of-a-kind hosts—all without leaving home.</p>
+        </div>
+            
+    )
+}
